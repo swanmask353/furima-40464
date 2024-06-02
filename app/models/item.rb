c6ac00e0adc4810dc92aaec964b16c3b9c3fef12
swanmask353 <youhei.suwa@gmail.com>
2024-06-02 19:57:00 +0900

    itemモデルの単体テストコード

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,4 +12,10 @@ class Item < ApplicationRecord
   validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   validates :description, presence: true
   validates :image, presence: true
+
+  validates :category_id, presence: true, numericality: { other_than: 0 }
+  validates :condition_id, presence: true, numericality: { other_than: 0 }
+  validates :prefecture_id, presence: true, numericality: { other_than: 0 }
+  validates :shipping_cost_id, presence: true, numericality: { other_than: 0 }
+  validates :shipping_duration_id, presence: true, numericality: { other_than: 0 }
 end
